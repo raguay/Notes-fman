71bb0986b458a3f5bfdbaeda32fbfcea5b298911
Richard Guay <raguay@customct.com>
2020-04-10 21:43:49 +0700

    testing....

--- notes/__init__.py
+++ notes/__init__.py
@@ -47,6 +47,7 @@ class Notes (DirectoryPaneCommand):
         # If the OpenWithEditor plugin is loaded, use it to edit 
         # the file. Otherwise, use the system edit command.
         #
+        show_alert(_get_thirdparty_plugins())
         if "OpenWithEditor" in _get_thirdparty_plugins():
             self.pane.run_command("my_open_with_editor", args={'url': cfNoteFile})
         else:
